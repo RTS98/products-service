88afcf473245c9dafa79462a6dc0c0e04e793c9c
robertstoia <robertstoia@yahoo.com>
2024-07-15 09:15:10 +0300

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,19 @@ import {
 import { ProductsModule } from './products/products.module';
 import ProductsMiddleware from './products/middlewares/products.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ProductsModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'products',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
--- src/products/config/typeORM.ts
+++ src/products/config/typeORM.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'products',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   synchronize: true,
   entities: ['dist/products/entities/*.entity{.ts,.js}'],
   migrations: ['dist/products/migrations/*{.ts,.js}'],
